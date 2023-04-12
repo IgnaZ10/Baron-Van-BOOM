56c7e11d5a50bd71f86ddf586a0fae1877effab9
IgnaZ10 <64873313+IgnaZ10@users.noreply.github.com>
2023-04-11 23:37:52 -0300

    Movimiento y Fisicas funcionando

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 12f;
+    public CharacterController Controller1;
 
-    void Update()
+    
+    private void Update()
     {
-        // Movimiento hacia adelante y hacia atrás
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        Controller1.Move(move * speed * Time.deltaTime);
+    }
+    /*void Movement()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += transform.forward * speed * Time.deltaTime;
+            transform.position += transform.forward * speed * Time.fixedDeltaTime;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= transform.forward * speed * Time.deltaTime;
+            transform.position -= transform.forward * speed * Time.fixedDeltaTime;
         }
 
-        // Movimiento hacia la izquierda y hacia la derecha
+        
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= transform.right * speed * Time.deltaTime;
+            transform.position -= transform.right * speed * Time.fixedDeltaTime;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.position += transform.right * speed * Time.deltaTime;
+            transform.position += transform.right * speed * Time.fixedDeltaTime;
         }
-    }
+    }*/
 
 }
 
