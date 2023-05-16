34b8e73ca25f9a2efa233e51b38c47ae6d4d35d1
IgnaZ10 <64873313+IgnaZ10@users.noreply.github.com>
2023-05-16 20:48:39 -0300

    Bomba Funciona, wiiiii!

--- Assets/Scripts/Bomba.cs
+++ Assets/Scripts/Bomba.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Bomba : MonoBehaviour
 {
     public float tiempoExplosion = 3f; // El tiempo que tarda la bomba en explotar
-    public float radioExplosion = 5f; // El radio de la explosión de la bomba
+    public float radioExplosion = 100f; // El radio de la explosión de la bomba
     
     private bool haExplotado = false; // Indica si la bomba ha explotado
     private float tiempoRestante; // Tiempo restante para que la bomba explote
@@ -29,15 +29,40 @@ public class Bomba : MonoBehaviour
     void Explode()
     {
         haExplotado = true;
-        
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, radioExplosion))
+        Vector3 vectorExplode = Vector3.zero;
+
+
+
+        for (int explosions = 0; explosions <= 3; explosions++)
         {
-            Debug.Log(hit.collider.name);
-            Debug.DrawLine(transform.position, transform.forward * radioExplosion);
-            
-        }
+            switch (explosions) {
+                case 0: vectorExplode = Vector3.back;
+                    break;
+                case 1:
+                    vectorExplode = Vector3.right;
+                    break;
+                case 2:
+                    vectorExplode = Vector3.forward;
+                    break;
+                case 3:
+                    vectorExplode = Vector3.left;
+                    break;
+
+            }
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(transform.position, vectorExplode, radioExplosion);
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit hit = hits[i];
+                BreakScript ruptura = hit.transform.GetComponent<BreakScript>();
+                if (ruptura)
+                Destroy(ruptura.transform.gameObject, 1f);
+
+            }
         
+                //Debug.DrawLine(transform.position, vectorExplode * radioExplosion);
+        }
         Destroy(gameObject, 0.5f);
     }
     
--- Assets/Scripts/BreakScript.cs
+++ Assets/Scripts/BreakScript.cs
@@ -6,20 +6,6 @@ public class BreakScript : MonoBehaviour
 {
 
 
-    private void FixedUpdate()
-    {
-        // Verifica si se ha aplicado una fuerza a este objeto
-        if (GetComponent<Rigidbody>().velocity.magnitude > 0)
-        {
-            // Busca un objeto llamado "BombPrefab(Clone)" en la escena
-            GameObject bomb = GameObject.Find("BombPrefab(Clone)");
-
-            // Si se encuentra la bomba, destruye este objeto
-            if (bomb != null)
-            {
-                Destroy(gameObject);
-            }
-        }
-    }
+    
 
 }
