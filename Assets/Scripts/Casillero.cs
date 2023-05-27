249410bf38458be9cfe3c6196d8f8819bdd03f34
dalmiroanashe <107286007+dalmiroanashe@users.noreply.github.com>
2023-05-26 23:57:46 -0300

    Arreglos pausa

--- Assets/Scripts/Casillero.cs
+++ Assets/Scripts/Casillero.cs
@@ -4,12 +4,22 @@ public class Casillero : MonoBehaviour
 {
     public GameObject objectToInstantiate; // El objeto que deseas instanciar
     public float yOffset = 1f; // Desplazamiento vertical
-    public int maxCajas = 3; // Límite de cajas
+    public int maxCajas = 10; // Límite de cajas
 
     private static int cajasInstanciadas = 0; // Contador de cajas instanciadas
 
+    private void OnDisable()
+    {
+        // Reiniciar la cantidad de cajas instanciadas al desactivar el objeto
+        cajasInstanciadas = 0;
+    }
     private void Update()
     {
+        // Verificar si el juego está pausado
+        if (Time.timeScale == 0f)
+        {
+            return; // Salir del método sin hacer nada si el juego está pausado
+        }
         if (Input.GetMouseButtonDown(0) && cajasInstanciadas < maxCajas)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
