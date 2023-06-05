fae62dfb7a5bf9800e54ecc50edee016b15c504a
dalmiroanashe <107286007+dalmiroanashe@users.noreply.github.com>
2023-06-05 17:01:30 -0300

    Mejora de IA y de mapa

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -7,23 +7,33 @@ public class EnemyMove : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public GameObject goalDestination;
+    public float chasingDistance = 5f; // Distancia para comenzar a perseguir al jugador
+    public float returnDistance = 30f; // Distancia para volver al punto inicial
     private GameObject player;
+    private Vector3 initialPosition;
     private bool isPlayerDestroyed = false;
 
     void Start()
     {
         player = GameObject.FindObjectOfType<PlayerController>().gameObject;
-        if (player != null)
-        {
-            navMeshAgent.destination = player.transform.position;
-        }
+        initialPosition = transform.position;
     }
 
     void Update()
     {
         if (!isPlayerDestroyed && player != null)
         {
-            navMeshAgent.destination = player.transform.position;
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            float distanceToInitialPosition = Vector3.Distance(transform.position, initialPosition);
+
+            if (distanceToPlayer <= chasingDistance)
+            {
+                navMeshAgent.destination = player.transform.position;
+            }
+            else if (distanceToInitialPosition > returnDistance)
+            {
+                navMeshAgent.destination = initialPosition;
+            }
         }
     }
 
@@ -48,6 +58,7 @@ public class EnemyMove : MonoBehaviour
         isPlayerDestroyed = true;
         navMeshAgent.enabled = false;
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -56,3 +67,4 @@ public class EnemyMove : MonoBehaviour
         }
     }
 }
+
