5610ef8a26b8a8e06c96e1cb465f49ef959235ab
IgnaZ10 <64873313+IgnaZ10@users.noreply.github.com>
2023-09-19 23:01:52 -0300

    ya esta funcionando en un bloque
    en el bloque original ya funciona el script, hay que pasarselo al resto

--- Assets/Scripts/TransparentBlocks.cs
+++ Assets/Scripts/TransparentBlocks.cs
@@ -4,35 +4,21 @@ using UnityEngine;
 
 public class TransparentBlocks : MonoBehaviour
 {
-    [SerializeField] private Color originalColor;
-    [SerializeField] public Renderer renderer;
-
-    private void Start()
-    {
-        // Al iniciar, obtenemos el componente Renderer y el color original del objeto
-        renderer = GetComponent<Renderer>();
-        originalColor = renderer.material.color;
-    }
+    public Material startMaterial;
+    public Material hoverMaterial;
+    bool mouseOver = false;
 
     private void OnMouseEnter()
     {
-        // Verificamos si el objeto tiene el tag "Unbreakable"
-        if (CompareTag("Unbreakable"))
-        {
-            // Cambiamos el color del material para que sea transparente
-            Color newColor = originalColor;
-            newColor.a = 0.5f; // Puedes ajustar este valor para cambiar la transparencia
-            renderer.material.color = newColor;
-        }
+        mouseOver = true;
+        GetComponent<Renderer>().material = hoverMaterial;
     }
 
+
     private void OnMouseExit()
+
     {
-        // Verificamos si el objeto tiene el tag "Unbreakable"
-        if (CompareTag("Unbreakable"))
-        {
-            // Restauramos el color original del material
-            renderer.material.color = originalColor;
-        }
+        mouseOver = false;
+        GetComponent<Renderer>().material = startMaterial;
     }
 }
